c29ded918ff028e8dcab06d48951634d54f1f02f
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-07-05 18:45:42 +0300

    Add project files.

---
+++ FindingDigitsAndLetters/Program.cs
@@ -0,0 +1,20 @@
+﻿using FindingDigitsAndLetters.Services;
+
+namespace FindingDigitsAndLetters;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        StartProgram();
+    }
+     static void StartProgram()
+    {
+        FindingLetterAndDigit findingDigitAndLetter = new FindingLetterAndDigit();
+        Console.Write("Enter string :");
+        string a = Console.ReadLine();
+        findingDigitAndLetter.CounterLetters(a);
+        findingDigitAndLetter.CounterDigits(a);
+    }
+
+}
---
+++ FindingDigitsAndLetters/Services/FindingLetterAndDigit.cs
@@ -0,0 +1,28 @@
+﻿namespace FindingDigitsAndLetters.Services;
+
+public class FindingLetterAndDigit
+{
+    int counterLetters;
+    int counterDigits;
+
+    public int CounterLetters(string str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            char ch = str[i];
+            if (char.IsLetter(ch)) ++counterLetters;
+        }
+        Console.WriteLine($"Number of Letters : {counterLetters}");
+        return counterLetters;
+    }
+    public int CounterDigits(string str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            char ch = str[i];
+            if (char.IsDigit(ch)) ++counterDigits;
+        }
+        Console.WriteLine($"Number of Digits : { counterDigits}");
+        return counterDigits;
+    }
+}
