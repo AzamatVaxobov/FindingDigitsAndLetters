67a051df5cc64e30e15e44666285d1aa8033cfd8
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-07-05 21:38:56 +0300

    Do you want to continue function added

--- FindingDigitsAndLetters/Program.cs
+++ FindingDigitsAndLetters/Program.cs
@@ -8,13 +8,21 @@ public class Program
     {
         StartProgram();
     }
-     static void StartProgram()
+    static void StartProgram()
     {
-        FindingLetterAndDigit findingDigitAndLetter = new FindingLetterAndDigit();
-        Console.Write("Enter string :");
-        string a = Console.ReadLine();
-        findingDigitAndLetter.CounterLetters(a);
-        findingDigitAndLetter.CounterDigits(a);
+        while (true)
+        {
+            FindingLetterAndDigit findingDigitAndLetter = new FindingLetterAndDigit();
+            Console.Write("Enter string :");
+            string a = Console.ReadLine();
+            findingDigitAndLetter.CounterLetters(a);
+            findingDigitAndLetter.CounterDigits(a);
+            string option;
+            Console.Write("Do you want to continue   Y/N :");
+            option = Console.ReadLine();
+            string[] options = { "Yes", "Y", "yes", "YES", "y" };
+            bool check = options.Any(o => o == option);
+            if (!check) break;
+        }
     }
-
 }
